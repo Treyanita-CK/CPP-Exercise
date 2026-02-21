12d4079432278a00c1a7e10cc1d3bce24a9a63f0
Treyanita Citra Karina <128873094+Treyanita-CK@users.noreply.github.com>
2026-02-21 21:22:34 +0700

    Initialize develop

---
+++ API/Migrations/20230622093423_tb_m_projectAkhir.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class tb_m_projectAkhir : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tb_m_account",
+                columns: table => new
+                {
+                    user_id = table.Column<string>(type: "char(5", nullable: false),
+                    user_name = table.Column<string>(type: "varchar(50)", nullable: false),
+                    email = table.Column<string>(type: "varchar(50)", nullable: false),
+                    password = table.Column<string>(type: "varchar(255)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_m_account", x => x.user_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tb_m_kategori",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nm_kategori = table.Column<string>(type: "varchar(50)", nullable: false),
+                    deskripsi = table.Column<string>(type: "varchar(255)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_m_kategori", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tb_m_komentar",
+                columns: table => new
+                {
+                    id_komen = table.Column<string>(type: "char(100)", nullable: false),
+                    tgl_komentar = table.Column<DateTime>(type: "datetime", nullable: false),
+                    isi_komentar = table.Column<string>(type: "varchar(255)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_m_komentar", x => x.id_komen);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tb_m_resep",
+                columns: table => new
+                {
+                    resep_id = table.Column<string>(type: "varchar(100)", nullable: false),
+                    nm_resep = table.Column<string>(type: "varchar(100)", nullable: false),
+                    deskripsi = table.Column<string>(type: "varchar(255)", nullable: false),
+                    bahan = table.Column<string>(type: "varchar(255)", nullable: false),
+                    jumlah = table.Column<string>(type: "varchar(20)", nullable: false),
+                    satuan = table.Column<string>(type: "varchar(20)", nullable: false),
+                    wkt_persiapan = table.Column<string>(type: "varchar(20)", nullable: false),
+                    wkt_memasak = table.Column<string>(type: "varchar(20)", nullable: false),
+                    kesulitan = table.Column<string>(type: "varchar(20)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_m_resep", x => x.resep_id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tb_m_account");
+
+            migrationBuilder.DropTable(
+                name: "tb_m_kategori");
+
+            migrationBuilder.DropTable(
+                name: "tb_m_komentar");
+
+            migrationBuilder.DropTable(
+                name: "tb_m_resep");
+        }
+    }
+}
---
+++ API/Models/Resep.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace API.Models;
+[Table("tb_m_resep")]
+public class Resep 
+{
+    // id
+    [Key, Column("resep_id", TypeName = "varchar(100)")]
+    public string IdResep { get; set; }
+
+    // nama resep
+    [Column("nm_resep", TypeName ="varchar(100)")]
+    public string NamaResep { get; set;}
+
+    // dekskripsi
+    [Column("deskripsi", TypeName ="varchar(255)")]
+    public string Deskripsi { get; set;}
+
+    // bahan
+    [Column("bahan", TypeName ="varchar(255)")]
+    public string Bahan { get; set;}
+
+    // jumlah bahan (2 siung bawang)
+    [Column("jumlah", TypeName = "varchar(20)")]
+    public string Jumlah { get; set; }
+
+    // satuan bahan (100 gr tepung)
+    [Column("satuan", TypeName = "varchar(20)")]
+    public string Satuan { get; set; }
+
+    // waktu persiapan
+    [Column("wkt_persiapan", TypeName ="varchar(20)")]
+    public string WaktuPersiapan { get; set;}
+
+    //waktu memasak
+    [Column("wkt_memasak", TypeName ="varchar(20)")]
+    public string WaktuMemasak { get; set;}
+
+    // kesulitan
+    [Column("kesulitan", TypeName = "varchar(20)")]
+    public string Kesulitan { get; set; }  
+}
