25dd47ca228a4716ac1e6f1fcc4c76b5001ec9d7
Treyanita Citra Karina <128873094+Treyanita-CK@users.noreply.github.com>
2026-02-21 21:22:33 +0700

    Initialize coba

---
+++ API/Models/Kategori.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace API.Models;
+
+[Table("tb_m_kategori")]
+public class Kategori
+{
+    // id
+    [Key, Column("id")]
+    public int Id { get; set; }
+
+    // nama kategori
+    [Column("nm_kategori", TypeName = "varchar(50)")]
+    public string NamaKategori { get; set; }
+
+    // deskripsi
+    [Column("deskripsi", TypeName = "varchar(255)")]
+    public string Deskripsi { get; set; }
+
+}
